39a109bb8645227feedb4aabd9324f955f06a83d
Daniele Di Sarli <danieleds0@gmail.com>
2022-10-26 17:31:16 +0200

    add trailingSlash config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,6 +12,7 @@ const config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
   favicon: "img/logo_black.png",
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
