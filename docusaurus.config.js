5eec6e2b6a1fe786fa4b0a4fc83e84ff67e1b495
Daniele Di Sarli <danieleds0@gmail.com>
2022-10-26 17:30:08 +0200

    update favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
-  favicon: "img/favicon.ico",
+  favicon: "img/logo_black.png",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
